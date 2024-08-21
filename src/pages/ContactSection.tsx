8dd3faa1b85048c403f48ef56c1765c718b67f75
Iris Yang <ixyang@mit.edu>
2024-08-20 18:49:23 -0700

    project section framework

--- src/pages/ContactSection.tsx
+++ src/pages/ContactSection.tsx
@@ -3,7 +3,7 @@ import SectionContainer from "../components/SectionContainer";
 export default function ContactSection() {
   return (
     <SectionContainer id="contact">
-      <h1 className="section-title-text">Contact Section</h1>
+      <h1 className="section-title-text">Contact</h1>
     </SectionContainer>
   );
 }
--- src/pages/ProjectSection.tsx
+++ src/pages/ProjectSection.tsx
@@ -1,9 +1,50 @@
 import SectionContainer from "../components/SectionContainer";
+import "./ProjectSection.css";
 
-export default function ProjectSection() {
+function ProjectSection() {
   return (
-    <SectionContainer id="project">
-      <h1 className="section-title-text">Project Section</h1>
-    </SectionContainer>
+    <section id="project" className="project-background">
+      <h1
+        className="section-title-text"
+        style={{ color: "white", marginBottom: 32 }}
+      >
+        Projects
+      </h1>
+      <div>
+        <Project title="Project #1">
+          this is what i did in this specific project, lots of miscellaneous
+          stuff. blah blah blah blah blah. i don't really know what else to add
+          in here right now. just an example i suppose we'll see how well this
+          even turns out.
+        </Project>
+        <Project title="Project #2">
+          yea i made another project, and what about it? you probably didn't
+          even know that i could even do something like that. yea, you're just a
+          fucking hater. and what about it, binch?
+        </Project>
+        <Project title="Project #3">
+          you won't believe what i did, but i made ANOTHER project... they said
+          i couldn't do it, but i did it anyway. that's what i fucking thought.
+          nevertheless, she persisted.
+        </Project>
+      </div>
+    </section>
   );
 }
+
+type ProjectProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+function Project(props: ProjectProps) {
+  const { title, children } = props;
+  return (
+    <div className="project-container">
+      <h1 className="project-name">{title}</h1>
+      <div className="project-description">{children}</div>
+    </div>
+  );
+}
+
+export default ProjectSection;
